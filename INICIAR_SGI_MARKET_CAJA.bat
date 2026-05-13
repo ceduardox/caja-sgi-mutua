b73090366ee932534b09efb5c6075727f5d3d37f
ceduardopc <seduardocallau@gmail.com>
2026-05-13 16:33:25 -0400

    Add local startup script

---
+++ INICIAR_SGI_MARKET_CAJA.bat
@@ -0,0 +1,9 @@
+@echo off
+setlocal
+cd /d "%~dp0"
+echo Iniciando SGI Market Caja...
+echo.
+echo Abre en el navegador: http://localhost:4173
+echo Para apagar el sistema, cierra esta ventana o presiona Ctrl+C.
+echo.
+npm.cmd start
